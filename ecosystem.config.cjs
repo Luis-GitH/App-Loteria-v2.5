3bf8b2b97b955c9bc7a08643166634f94165c9b6
Luis <luis.lopez.gd@outlook.es>
2025-12-08 22:41:42 +0100

    exportar excel y revision cron

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -53,18 +53,18 @@ module.exports = {
       args: "--all",
       cwd: './',
       error_file: "./logs/update-today-errores.log",
-      cron_restart: "30 22 * * *", // todos los disa  a las 22:30
+      cron_restart: "40 22 * * *", // todos los disa  a las 22:30
       autorestart: false,
       
     },
      {  
       name: "verify-week",
       script: "verify-week.js",
-      args: "--week --all",
+      args: "--week --all --users",
       cwd: './',
       time: true,
       error_file: "./logs/verify-week-errores.log",
-      cron_restart: "40 22 * * 7", // a "40 22 * * 7" // las 10:45 los domingos
+      cron_restart: "45 22 * * 7", // a "40 22 * * 7" // las 10:45 los domingos
       autorestart: false
     },
 
